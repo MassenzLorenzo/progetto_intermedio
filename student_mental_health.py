bc19773439f716598add6c63b78c2a05262c41e5
Massenz Lorenzo <massenzlorenzo2006@gmail.com>
2026-02-27 17:45:03 +0100

    add: csv of the dataset; pdf of the project; data undestanding and preprocessing 4.2 (student_mental_health.py)

--- student_mental_health.py
+++ student_mental_health.py
@@ -21,3 +21,69 @@ Variabili Chiave Utilizzate:
 - Anxious Before Exams (Categoriale dicotomica): Ansia prima degli esami (Yes/No).
 - Gender (Categoriale nominale): Genere dello studente.
 """
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# ==========================================
+# 4.2 DATA UNDERSTANDING E PREPROCESSING
+# ==========================================
+
+# 1. Caricamento del dataset
+df = pd.read_csv('csv/Student_Mental_Health.csv')
+
+# Visualizziamo le prime righe
+print("--- Prime 5 righe del dataset ---")
+print(df.head())
+
+# Verifichiamo le informazioni generali
+print("\n--- Informazioni sul dataset ---")
+print(df.info())
+
+# 2. Analisi dei valori mancanti (Missing Values)
+print("\n--- Conteggio dei valori mancanti per colonna ---")
+print(df.isnull().sum())
+
+""" 
+Valori mancanti: 0, quindi non è necessario alcun trattamento per i missing values.
+comando per listwise deletion: df = df.dropna(subset=colonne_chiave)
+"""
+
+# Rinominiamo le colonne per una maggiore chiarezza e coerenza con le domande statistiche
+df = df.rename(columns={
+    'Sleep Duration (hrs)': 'sleep_hours',
+    'Physical Activity (hrs/week)': 'sport_hours',
+    'Anxious Before Exams': 'anxiety_status',
+    'Gender': 'gender'
+})
+
+# Selezioniamo solo le colonne che ci servono per le nostre domande
+colonne_chiave = ['sleep_hours', 'sport_hours', 'anxiety_status', 'gender']
+
+# 3. Gestione Outlier (Valori anomali)
+# Visualizziamo i boxplot per identificare visivamente eventuali anomalie
+plt.figure(figsize=(12, 6))
+
+# utilizziamo subplot per disporre i grafici affiancati 
+plt.subplot(1, 2, 1) # righe, colonne, indice del grafico
+sns.boxplot(y=df['sleep_hours'], color='skyblue')
+plt.title('Distribuzione Ore di Sonno')
+plt.ylabel('Ore')
+
+plt.subplot(1, 2, 2) # righe, colonne, indice del grafico
+sns.boxplot(y=df['sport_hours'], color='lightgreen')
+plt.title('Distribuzione Attività Fisica (ore/settimana)')
+plt.ylabel('Ore')
+
+plt.tight_layout() 
+plt.show()
+
+"""
+Attraverso l'ispezione visiva dei boxplot per le variabili sleep_hours e sport_hours,
+si osserva la totale assenza di outlier (rappresentati graficamente da punti isolati oltre i baffi).
+Questo indica che il dataset è estremamente pulito e che le abitudini dichiarate dagli studenti
+sono verosimili e prive di anomalie statistiche.
+Di conseguenza, non è stato necessario procedere alla rimozione di alcuna osservazione, 
+mantenendo l'integrità dell'intero campione originale per le analisi successive.
+"""
